d8a20ac62c1a804c38c418255ff0cb2bc7ba179c
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-08-07 18:01:34 +0100

    STUDENTEXERCISE

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
+import fpdf
 import pandas as pd
 from fpdf import FPDF
+
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
 pdf.set_auto_page_break(auto=False, margin=0)
@@ -20,12 +22,22 @@ for index, row in df.iterrows():
     pdf.set_text_color(180, 180, 180)
     pdf.cell(w=0, h=10, txt=text, align="R", ln=1)
 
+# STUDENT EXERCISE
+#    for x in range(20):
+#        y = (14*x)+21
+#        pdf.line(x1=10, y1=y, x2=200, y2=y)
+
     pages = int(row["Pages"])
-    for x in range(pages-1):
+    for y in range(pages-1):
         pdf.add_page()
         pdf.ln(277)
         pdf.set_font(family="Times", size=8, style="I")
         pdf.set_text_color(180, 180, 180)
         pdf.cell(w=0, h=10, txt=text, align="R", ln=1)
+
+# STUDENT EXERCISE
+#        for x in range(20):
+#            y = (14 * x) + 21
+#            pdf.line(x1=10, y1=y, x2=200, y2=y)
 
 pdf.output("output.pdf")
