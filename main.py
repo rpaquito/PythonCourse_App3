15d11a0aa0a9b6ee5457697146b37003754507ab
Ricardo Paquito <ricardo.paquito@radicant.com>
2023-08-07 17:47:55 +0100

    1st

--- main.py
+++ main.py
@@ -2,15 +2,30 @@ import pandas as pd
 from fpdf import FPDF
 
 pdf = FPDF(orientation="P", unit="mm", format="A4")
+pdf.set_auto_page_break(auto=False, margin=0)
 
 df = pd.read_csv("topics.csv")
 
 for index, row in df.iterrows():
     pdf.add_page()
+
     text = row["Topic"]
-    pdf.set_font(family="Times", size=12, style="B")
-    pdf.set_text_color(100,100,100)
-    pdf.cell(w=0, h=24, txt=text, align="L", ln=1)
-    pdf.line(x1=10, x2=21, y1=200, y2=21)
+    pdf.set_font(family="Times", size=24, style="B")
+    pdf.set_text_color(100, 100, 100)
+    pdf.cell(w=0, h=12, txt=text, align="L", ln=1)
+    pdf.line(x1=10, y1=21, x2=200, y2=21)
+
+    pdf.ln(265)
+    pdf.set_font(family="Times", size=8, style="I")
+    pdf.set_text_color(180, 180, 180)
+    pdf.cell(w=0, h=10, txt=text, align="R", ln=1)
+
+    pages = int(row["Pages"])
+    for x in range(pages-1):
+        pdf.add_page()
+        pdf.ln(277)
+        pdf.set_font(family="Times", size=8, style="I")
+        pdf.set_text_color(180, 180, 180)
+        pdf.cell(w=0, h=10, txt=text, align="R", ln=1)
 
 pdf.output("output.pdf")
